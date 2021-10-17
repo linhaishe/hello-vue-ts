6a1deaaf5c542edad40cc347869b8b6116c5ead4
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-10-17 21:16:33 +0800

    fix eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', '@vue/typescript/recommended'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module', //指定源代码存在的位置，script | module，默认为script。
   },
-  rules: { semi: 2 },
+  rules: { semi: 1 },
 }
